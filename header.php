e512b23ee441e3d185f6cd9a8c53771663025549
Tomy-my <azerouto@gmail.com>
2021-04-27 16:39:18 +0200

    Structure du header (menu)

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/font.css" />
+		<link rel="stylesheet" href="css/responsive.css" />
+		<title>Ampoule - Dashboard : <?php echo $titre; ?></title>
+	</head>
+    <body>
+        <nav>
+            <div class="menu">
+                <div class="listing_menu">
+                    <img id="logo" src="#" href="home.php" alt="Logo du site">
+                    <ui>
+                        <li>Tableau de bord</li>
+                        <li>Historique</li>
+                        <li>Réseau</li>
+                        <li>Incident</li>
+                        <li>Bug</li>
+                    </ui>
+                    <buttoun id="deconnexion">Déconnexion</buttoun>
+                </div>
+            </div>
+        </nav>
