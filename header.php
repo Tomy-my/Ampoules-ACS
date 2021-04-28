bbfa91ff9a619da328e869b89aaf1ec6c5df15fc
Tomy-my <azerouto@gmail.com>
2021-04-28 15:29:39 +0200

    All menu responsive

--- header.php
+++ header.php
@@ -17,12 +17,27 @@
                 <img id="logo" src="img/logo/Logo.svg" alt="Logo du site">
                 <ul>
                     <li id="home"><i class="fas fa-home"></i> <a href="index.php">Tableau de bord</a></li>
-                    <li><i class="fas fa-history"></i> <a href="index.php">Historique</a></li>
-                    <li><i class="fas fa-wifi"></i> <a href="index.php">Réseau</a></li>
-                    <li><i class="fas fa-exclamation-triangle"></i> <a href="index.php">Incident</a></li>
-                    <li><i class="fas fa-bug"></i> <a href="index.php">Bug</a></li>
+                    <li id="history"><i class="fas fa-history"></i> <a href="index.php">Historique</a></li>
+                    <li id="reseau"><i class="fas fa-wifi"></i> <a href="index.php">Réseau</a></li>
+                    <li id="incident"><i class="fas fa-exclamation-triangle"></i> <a href="index.php">Incident</a></li>
+                    <li id="bug"><i class="fas fa-bug"></i> <a href="index.php">Bug</a></li>
                     <li><i class="fas fa-power-off"></i> <buttoun id="deconnexion">Déconnexion</buttoun> </li>
                 </ul>
+                <hr>
+                <div class="prst">
+                    <h1>Le<span>k</span>ki Ampoule ~</h1>
+                    <p>
+                        Est un Dashboard développé par Tomy SOTTY dans le but
+                        d'aider les concieres des immeubles à entretenir l'éclairage
+                        communal.
+                    </p>
+                </div>
+                <div class="social">
+                    <a id="git" href="https://github.com/Tomy-my"><i class="fab fa-github"></i></a>
+                    <a id="insta" href="https://www.instagram.com/_dakor/"><i class="fab fa-instagram"></i></a>
+                    <a id ="lock" href="https://www.twitch.tv/locklear"><i class="fab fa-twitch"></i></a>
+                </div>
+                <hr id="footer">
             </div>
         <!-- Menu secondaire-->
             <div class="listing_menu_secondaire">
@@ -50,7 +65,7 @@
                 </a>
                 <burger id="burger_menu">
                     <ul>
-                        <li><i class="fas fa-home"></i> <a href="index.php">Tableau de bord</a></li>
+                        <li style="margin-top: -80px;"><i class="fas fa-home"></i> <a href="index.php">Tableau de bord</a></li>
                         <li style="padding-right: 46px;"><i class="fas fa-history"></i> <a href="index.php">Historique</a></li>
                         <li style="padding-right: 70px;"><i class="fas fa-wifi"></i> <a href="index.php">Réseau</a></li>
                         <li style="padding-right: 63px;"><i class="fas fa-exclamation-triangle"></i> <a href="index.php">Incident</a></li>
