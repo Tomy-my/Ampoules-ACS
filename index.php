86401cb501fd1bdc351c29c9093994d954ec5d47
Tomy-my <azerouto@gmail.com>
2021-04-30 08:59:42 +0200

    petite modification

--- index.php
+++ index.php
@@ -3,45 +3,56 @@
 $titre = "Home";   //Je nomme le titre de la page
 
 include('header.php');  //J'ammène la page header
+
+include('controller/formulaire.php')
 ?>
 
 
 <div class="container_form">
     <div class="add_form">
         <div class="bandeau">
-            <h1>Ajout d'ampoule &nbsp<i class="far fa-lightbulb"></i></h1>
+            <h1>Ajout d'ampoule</h1>
         </div>
-        <form class="add" action="controller/formulaire.php" method="post">
-            
-            <h1 id="titre">Précisez la date d'ajout de l'ampoule</h1>
-            <input class="date" type="date" id="start" name="date_lekki" value="2021-04-28" min="2021-01-01" max="2030-12-31">
-            
-            <h1 id="titre_sp">Indiqué l'étage</h1>
-            <div class='ctrl'>
-                <div class='ctrl__button ctrl__button--decrement'>&ndash;</div>
-                <div class='ctrl__counter'>
-                    <input class='ctrl__counter-input' name="upstairs" maxlength='10' type='text' value='0'>
-                    <div class='ctrl__counter-num'>0</div>
+        <div class="int">
+            <div class="gauche">
+                <form class="add" action="index.php" method="post">
+                    
+                    <h1 id="titre">Précisez la date d'ajout de l'ampoule</h1>
+                    <input class="date" type="date" id="start" name="date_lekki" value="2021-04-28" min="2021-01-01" max="2030-12-31">
+                    
+                    <h1 id="titre_sp">Indiqué l'étage</h1>
+                    <div class='ctrl'>
+                        <div class='ctrl__button ctrl__button--decrement'>&ndash;</div>
+                        <div class='ctrl__counter'>
+                            <input class='ctrl__counter-input' name="upstairs" maxlength='10' type='text' value='0'>
+                            <div class='ctrl__counter-num'>0</div>
+                        </div>
+                        <div class='ctrl__button ctrl__button--increment'>+</div>
+                    </div>
+                    
+                    <h1 id="titre_sp">Où se situe l'ampoule ?</h1>
+                    <div class="select">
+                        <select  id="slct" name="position">
+                            <option selected disabled>Veuillez choisir une option</option>
+                            <option value="Left">Côté Gauche</option>
+                            <option value="Right">Côté Droit</option>
+                            <option value="Middle">Au Millieu</option>
+                            <option value="Back">Au Fond</option>
+                        </select>
+                    </div>
+                    
+                    <h1 id="titre">Définissait le prix</h1>
+                    <input class="prix" type="text" onkeypress="return verif(event);" name="price" placeholder="€">           
+            </div>
+            <div class="droite">
+                <div class="amp">
+                    <i class="far fa-lightbulb"></i>
+                    <button class="send">Allumer !</button>
+                    <div id="snackbar">Some text some message..</div>
                 </div>
-                <div class='ctrl__button ctrl__button--increment'>+</div>
+            </form>
             </div>
-            
-            <h1 id="titre_sp">Où se situe l'ampoule ?</h1>
-            <div class="select">
-                <select  id="slct" name="position">
-                    <option selected disabled>Veuillez choisir une option</option>
-                    <option value="Left">Côté Gauche</option>
-                    <option value="Right">Côté Droit</option>
-                    <option value="Middle">Au Millieu</option>
-                    <option value="Back">Au Fond</option>
-                </select>
-            </div>
-            
-            <h1 id="titre">Définissait le prix</h1>
-            <input class="prix" type="text" onkeypress="return verif(event);" name="price" placeholder="€">
-			
-            <button class="send">Envoyer !</button>
-		</form>
+        </div>
     </div>
 
     <div class="mod_form">
